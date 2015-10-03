7589c13bf7d5da348fb4fddcb5c7c94c21d21324
matt <matthew.l.lee@vanderbilt.edu>
2015-10-03 04:21:44 -0500

    responding with hard-coded user statuses (free/busy)

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var _ = require("underscore");
+
+var friends = {
+    "matt":"free",
+    "jason":"busy"
+}
+
+//fs.writeFile( "friends_list.json", JSON.stringify( friends ), "utf8"); //callback is optional 4th param
+
+// And then, to read it...
+//myJson = require("./filename.json");
+
 
 app.get('/', function (req, res) {
   res.send('Hello Vandy!');
 });
 
-app.get('/user', function (req, res) {
-  res.send('You got a user');
+// Handle GET to fetch user information
+app.get('/user/:username', function(req, res) {
+    if (friends[req.params.username] != undefined){
+        console.log("user found")
+        res.send(friends[req.params.username])
+    }
+    else {
+        console.log("not found")
+        res.sendStatus(404)
+    }
+});
+
+app.post('/user', function(req, res) {
+    var name = req.body.name,
+        availability = req.body.availability;
 });
 
 var port = process.env.PORT || 8080;
