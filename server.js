f304e1aaa9c5f39201d7355f1ffd5b87b70ef4a1
matt <matthew.l.lee@vanderbilt.edu>
2015-10-03 03:33:26 -0500

    added basic get to node server

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hey VandyHacks!');
+});
+
+app.get('/user', function (req, res) {
+  res.send('You got a user');
+});
+
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
